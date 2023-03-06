72ebae0d265951ff8007d1c1522df1bc633c70bc
whydoifly <netpreecher@gmail.com>
2023-03-06 20:19:16 -0300

    Fetching data for project detail page

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,8 +1,19 @@
+import { useParams } from 'react-router-dom';
+import { useDocument } from '../../hooks/useDocument';
+
 // styles 
 import './Project.css';
 
 export default function Project() {
+  const { id } = useParams();
+  const { error, document } = useDocument('projects', id);
+
+  if (error) return <p className='error'>{error}</p>;
+
+  if (!document) return <p>Loading...</p>;
   return (
-    <div>Project</div>
+    <div className='project-details'>
+      <h1>{document.name}</h1>
+    </div>
   )
 }
