439b023c2f3cfce9d0267c4012ad10697508f309
whydoifly <netpreecher@gmail.com>
2023-03-07 11:47:53 -0300

    Listing Comments

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Avatar from '../../components/Avatar';
 import { timestamp } from '../../firebase/config';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useFirestore } from '../../hooks/useFirestore';
@@ -29,6 +30,26 @@ export default function ProjectComments({ project }) {
   return (
     <div className='project-comments'>
       <h4>Project comments</h4>
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => {
+            return (
+              <li kref={comment.id}>
+                <div className='comment-author'>
+                  <Avatar src={comment.photoURL} />
+                  <p>{comment.displayName}</p>
+                </div>
+                <div className='comment-date'>
+                  <p>date here</p>
+                </div>
+                <div className='comment-content'>
+                  <p>{comment.content}</p>
+                </div>
+              </li>
+            );
+          })}
+      </ul>
+
       <form className='add-comment' onSubmit={handleSubmit}>
         <label>
           <span>Add new comment:</span>
